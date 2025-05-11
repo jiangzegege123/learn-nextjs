61852457a49da1977aa77b3d8d638c51256643e6
jiangzegege123 <161683111+jiangzegege123@users.noreply.github.com>
2025-05-11 16:23:10 +1000

    push

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <div>Hello from the index page</div>;
+import { prisma } from "./utils/db";
+async function getData() {
+  const data = await prisma.blogPost.findMany({
+    select: {
+      title: true,
+      content: true,
+      imageUrl: true,
+      authorImage: true,
+      id: true,
+    },
+  });
+  return data;
+}
+
+export default async function Page() {
+  const data = getData(); // ⬅️ 这里才执行初始化！
+  const posts = await prisma.blogPost.findMany();
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <pre>{JSON.stringify(posts, null, 2)}</pre>
+    </div>
+  );
 }
